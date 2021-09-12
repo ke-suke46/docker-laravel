122b59b4cdbdbaa8de1891375a2d8b4394533a88
ke-suke46 <kekeke4060@icloud.com>
2021-09-12 16:58:55 +0900

    model,contoller,viewの作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::get('todo', [TodoController::class, 'index'])->name('todo.index');
+Route::get('todo/create',[TodoController::class, 'create'])->name('todo.create');
+Route::post('todo', 'TodoController@store');
+Route::get('todo/{id}', 'TodoController@show');
+Route::get('todo/{id}/edit', 'TodoController@edit');
+Route::put('todo/{id}', 'TodoController@update');
+Route::delete('todo/{id}', 'TodoController@destroy');
 });
